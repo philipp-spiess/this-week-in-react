7a346f01e5201c75e85a01bc386ae15b71ab35fa
Philipp Spiess <hello@philippspiess.com>
2019-02-01 12:02:53 +0100

    Build improvements

--- static.config.js
+++ static.config.js
@@ -8,7 +8,10 @@ const latestIssue = issues[0].issue;
 const firstIssue = issues[issues.length - 1].issue;
 
 export default {
-  siteRoot: "https://this-week-in-react.org",
+  siteRoot:
+    process.env.NODE_ENV === "production"
+      ? "http://localhost:3000"
+      : "https://this-week-in-react.org",
   getSiteData: () => ({
     title: "This Week in React"
   }),
@@ -43,13 +46,17 @@ export default {
 };
 
 function parseIssue(path) {
-  const html = fs.readFileSync(path).toString();
-  const match = /This Week in React – Issue (\d+) – (.*)/.exec(html);
+  try {
+    const html = fs.readFileSync(path).toString();
+    const match = /This Week in React – Issue (\d+) – (.*)/.exec(html);
 
-  const issue = parseInt(match[1]);
-  const date = new Date(match[2]);
+    const issue = parseInt(match[1]);
+    const date = new Date(match[2]);
 
-  return { issue, date, html };
+    return { issue, date, html };
+  } catch (error) {
+    throw new Error(`Error parsing issue ${path}\n\n${error.message}`);
+  }
 }
 
 function utcDate(date) {
